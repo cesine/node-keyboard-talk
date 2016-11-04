9f98074d9f2b20d5470abccd0ea418fd4e5348f8
justin j. moses <justinjmoses@gmail.com>
2016-11-04 17:35:13 -0400

    Support for badwords and tweet stream cleanup

--- examples/14_rx_twitter.js
+++ examples/14_rx_twitter.js
@@ -2,6 +2,7 @@
 const Rx = require('node-keyboard/lib/rx')()
 const chalk = require('chalk')
 const twitterSentiment = require('twitter-sentiment')
+const profanity = new (require('bad-words'))()
 
 const play = require('node-keyboard/lib/mappers').play
 
@@ -13,9 +14,13 @@ module.exports = ({ track, minFollowers }) => {
 
     const tweets = Rx.Observable.fromEvent(readable, 'data')
 
-    tweets.subscribe(tweet => {
+    const cleaner = profanity.clean.bind(profanity)
+
+    tweets.finally(() => readable.emit('destroy')).subscribe(tweet => {
         process.stdout.write(`score: ${tweet.sentiment.score}, comparative: ${tweet.sentiment.comparative.toFixed(2)}`)
-        process.stdout.write(` ${chalk.green(tweet.sentiment.positive.join(' '))} ${chalk.red(tweet.sentiment.negative.join(' '))}\n`)
+
+        process.stdout.write(` ${chalk.green(tweet.sentiment.positive.map(cleaner).join(' '))}`)
+        process.stdout.write(` ${chalk.red(tweet.sentiment.negative.map(cleaner).join(' '))}\n`)
     })
 
     return tweets.map(tweet => 48 + tweet.sentiment.score).flatMap(note => [ note, note + 12 ]).subscribe(play)
